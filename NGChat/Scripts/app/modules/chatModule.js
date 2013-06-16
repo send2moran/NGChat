4f04c9f9d40944323f7eb3b7b09ae5406541d542
pwozny <vineq86@gmail.com>
2013-06-16 17:00:41 +0200

    Chat module refactor

--- NGChat/Scripts/app/modules/chatModule.js
+++ NGChat/Scripts/app/modules/chatModule.js
@@ -2,49 +2,45 @@
 
 angular
     .module('chat')
-    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-        var resolves = {
+    .config(['$routeProvider', function ($routeProvider) {
+        var resolvers = {
             auth: ['$q', '$location', 'userFactory', 'enumFactory', function ($q, $location, userFactory, enumFactory) {
                 var delay = $q.defer(),
-                    isHomepage = $location.url() === '/';
+                    isHomepage = $location.url() === '/',
+                    isChat = $location.url() === '/chat';
+
+                function redirectIf(condition, redirectPath) {
+                    if (condition) {
+                        delay.reject();
+                        $location.path(redirectPath);
+                    } else
+                        delay.resolve();
+                };
 
                 console.log('user.authState in resolver: ' + userFactory.authState);
 
-                if (userFactory.authState === enumFactory.authState.unknown) {
-                    userFactory.checkIfLogged()
-                        .success(function (data, status, headers, config) {
-                            if (data.success) {
-                                if ($location.url() !== '/chat') {
-                                    delay.reject();
-                                    $location.path('/chat');
-                                } else
-                                    delay.resolve();
-                            } else {
-                                if (!isHomepage) {
-                                    delay.reject();
-                                    $location.path('/');
-                                } else
-                                    delay.resolve();
-                            }
-                        })
-                        .error(function (data, status, headers, config) {
-                            if (!isHomepage) {
-                                delay.reject();
-                                $location.path('/');
-                            } else
-                                delay.resolve();
-                        });
-                } else if (userFactory.authState === enumFactory.authState.notAuthenticated) {
-                    delay.resolve();
+                switch (userFactory.authState) {
+                    case enumFactory.authState.unknown:
+                        userFactory.checkIfLogged()
+                            .success(function (data, status, headers, config) {
+                                if (data.success)
+                                    redirectIf(!isChat, '/chat');
+                                else
+                                    redirectIf(!isHomepage, '/');
+                            })
+                            .error(function (data, status, headers, config) {
+                                redirectIf(!isHomepage, '/');
+                            });
 
-                    if (!isHomepage)
-                        $location.path('/');
-                } else if (userFactory.authState === enumFactory.authState.authenticated) {
-                    if (isHomepage) {
-                        delay.reject();
-                        $location.path('/chat');
-                    } else
-                        delay.resolve();
+                        break;
+                    case enumFactory.authState.notAuthenticated:
+                        redirectIf(!isHomepage, '/');
+
+                        break;
+                    case enumFactory.authState.authenticated:
+                        redirectIf(!isChat, '/chat');
+
+                        break;
                 }
 
                 return delay.promise;
@@ -56,19 +52,17 @@ angular
                 controller: 'LogInController',
                 templateUrl: '/Home/LogIn',
                 resolve: {
-                    auth: resolves.auth
+                    auth: resolvers.auth
                 }
             })
             .when('/chat', {
                 controller: 'HomeController',
                 templateUrl: '/Home/Main',
                 resolve: {
-                    auth: resolves.auth
+                    auth: resolvers.auth
                 }
             })
             .otherwise({ redirectTo: '/' });
-
-        $locationProvider.hashPrefix('!');
     }])
     .run(['$location', '$q', '$timeout', '$rootScope', 'userFactory', function ($location, $q, $timeout, $rootScope, userFactory) {
         $rootScope.$on("$routeChangeStart", function (current, next) {
